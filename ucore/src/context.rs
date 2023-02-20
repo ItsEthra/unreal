3ecfa95d84e4ba8948c387b0ca146b6e2ceaba6a
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-02-20 17:26:03 +0300

    Added a lot of generics

--- ucore/src/context.rs
+++ ucore/src/context.rs
@@ -1,23 +1,41 @@
 #![allow(dead_code)]
 
-use std::ptr::NonNull;
+use crate::FNamePool;
+use std::{mem::transmute, ptr::NonNull};
 
-static mut CONTEXT: Option<GlobalContext> = None;
+static mut CONTEXT: Option<GlobalContext<0>> = None;
 
-pub struct GlobalContext {
+pub struct GlobalContext<const STRIDE: usize> {
     objects: NonNull<()>,
     names: NonNull<()>,
 }
 
-impl GlobalContext {
-    pub fn new_init_global(objects: NonNull<()>, names: NonNull<()>) -> &'static GlobalContext {
+impl<const STRIDE: usize> GlobalContext<STRIDE> {
+    pub fn new_init_global(
+        objects: NonNull<()>,
+        names: NonNull<()>,
+    ) -> &'static GlobalContext<STRIDE> {
         unsafe {
             if CONTEXT.is_none() {
-                CONTEXT = Some(Self { objects, names });
-                CONTEXT.as_ref().unwrap()
+                CONTEXT = Some(transmute(Self { objects, names }));
+                Self::get()
             } else {
                 panic!("GlobalContext is already initialized")
             }
         }
+    }
+
+    #[inline]
+    pub(crate) fn get() -> &'static GlobalContext<STRIDE> {
+        unsafe {
+            let s0: &'static GlobalContext<0> =
+                CONTEXT.as_ref().expect("GlobalContext is not initialized");
+            transmute(s0)
+        }
+    }
+
+    #[inline]
+    pub(crate) fn name_pool(&self) -> &FNamePool<STRIDE> {
+        unsafe { self.names.cast::<FNamePool<STRIDE>>().as_ref() }
     }
 }
